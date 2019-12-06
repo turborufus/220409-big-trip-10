7669e22ac8645aacac6f8c335dc66ecc582cc8bc
Alexandra Samonenko <turborufus@gmail.com>
2019-12-06 23:21:48 +0300

    Перенесла Types в const и разбила на 2 массива

--- src/const.js
+++ src/const.js
@@ -10,5 +10,10 @@ export const TypePlaceholder = {
   'train': `Train to `,
   'transport': `Transport to `
 };
+export const TransferTypes = [`bus`, `drive`, `flight`, `ship`, `taxi`, `train`, `transport`];
+export const ActivityTypes = [`check-in`, `restaurant`, `sightseeing`];
 
 export const Destinations = [`Geneva`, `Amsterdam`, `Saint Petersburg`, `Moscow`, `Brugge`];
+
+export const MonthNames = [`Jan`, `Feb`, `Mar`, `Apr`, `May`, `Jun`, `Jul`, `Aug`, `Sep`, `Oct`, `Nov`, `Dec`];
+
--- src/mock/event.js
+++ src/mock/event.js
@@ -1,23 +1,23 @@
 import {getRandomArrayItem, getRandomIntegerNumber, getDateDaysAgo, getRandomDate} from "../utils.js";
 import {generateOffers} from "./offer.js";
 import {generateDestination} from "./destination.js";
-import {Destinations} from "../const.js";
-
-const Types = [`bus`, `check-in`, `drive`, `flight`, `restaurant`,
-  `ship`, `sightseeing`, `taxi`, `train`, `transport`];
+import {Destinations, TransferTypes, ActivityTypes} from "../const.js";
 
 const generateEvent = () => {
   const daysAgo = getRandomIntegerNumber(1, 10);
   const startDate = getRandomDate(getDateDaysAgo(daysAgo), new Date());
   const stopDate = getRandomDate(startDate, new Date());
+  const types = TransferTypes.concat(ActivityTypes);
+  const type = getRandomArrayItem(types);
+  const selectedOffers = generateOffers(type).filter(Math.random > 0.5).slice(0, 2);
 
   return {
-    type: getRandomArrayItem(Types),
+    type,
     destination: generateDestination(getRandomArrayItem(Destinations)),
     start: startDate,
     stop: stopDate,
     price: getRandomIntegerNumber(1, 15) * 10,
-    offers: new Set(generateOffers(getRandomIntegerNumber(0, 2)))
+    offers: new Set(selectedOffers)
   };
 };
 
