310a272fbb82c0ebafdef335f0b94624ea99b076
Alexandra Samonenko <turborufus@gmail.com>
2019-12-06 21:55:05 +0300

    + фильтры

--- src/components/filter.js
+++ src/components/filter.js
@@ -1,21 +1,19 @@
-export const createFilterTemplate = () => {
+const createFilterMarkup = (filter, isChecked) => {
+  return (`
+    <div class="trip-filters__filter">
+      <input id="filter-${filter}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${filter}" checked>
+      <label class="trip-filters__filter-label" for="filter-${filter}">${filter}</label>
+      ${isChecked ? `checked` : ``}
+    </div>
+  `);
+};
+
+export const createFilterTemplate = (filters) => {
+  const filtersMarkup = filters.map((it, i) => createFilterMarkup(it, i === 0)).join(`\n`);
+
   return (`
       <form class="trip-filters" action="#" method="get">
-          <div class="trip-filters__filter">
-              <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked>
-              <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
-          </div>
-  
-          <div class="trip-filters__filter">
-              <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
-              <label class="trip-filters__filter-label" for="filter-future">Future</label>
-          </div>
-  
-          <div class="trip-filters__filter">
-              <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past">
-              <label class="trip-filters__filter-label" for="filter-past">Past</label>
-          </div>
-  
+          ${filtersMarkup}  
           <button class="visually-hidden" type="submit">Accept filter</button>
       </form>
   `);
