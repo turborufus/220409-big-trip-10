dd37ff34475f29c3373357751016ba837e577d8b
Alexandra Samonenko <turborufus@gmail.com>
2019-12-06 22:10:40 +0300

    + menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,8 +1,14 @@
-export const createTripMenuTemplate = () => {
+const createMenuTabMarkup = (tabName, isActive) => {
+  return (`
+    <a class="trip-tabs__btn  ${isActive ? `trip-tabs__btn--active` : ``}" href="#">${tabName}</a>
+  `);
+};
+
+export const createTripMenuTemplate = (menuTabs) => {
+  const menuTabsMarkup = menuTabs.map((it, i) => createMenuTabMarkup(it, i === 0)).join(`\n`);
   return (`
       <nav class="trip-controls__trip-tabs  trip-tabs">
-          <a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Table</a>
-          <a class="trip-tabs__btn" href="#">Stats</a>
+          ${menuTabsMarkup}
       </nav>
   `);
 };
