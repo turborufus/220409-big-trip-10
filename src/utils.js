659352cfb4093444ced6d58a2d06e8e7497aaa64
Alexandra Samonenko <turborufus@gmail.com>
2019-12-08 23:16:32 +0300

    smallfix

--- src/utils.js
+++ src/utils.js
@@ -53,8 +53,6 @@ export const formatDate = (date) => {
   const year = date.getFullYear();
   const month = (date.getMonth() < 9) ? `0${date.getMonth() + 1}` : `${date.getMonth() + 1}`;
   const day = (date.getDate() < 10) ? `0${date.getDate()}` : `${date.getDate()}`;
-  const minute = (date.getMinutes() < 10) ? `0${date.getMinutes()}` : `${date.getMinutes()}`;
-  const hour = (date.getHours() < 10) ? `0${date.getHours()}` : `${date.getHours()}`;
 
   return `${year}-${month}-${day}`;
 };
