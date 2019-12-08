dc7d88d4df8beae83539ba9b3afe662497bc9bb6
Alexandra Samonenko <turborufus@gmail.com>
2019-12-08 23:15:25 +0300

    + разбиение на дни

--- src/components/trip-day-item.js
+++ src/components/trip-day-item.js
@@ -1,13 +1,14 @@
 import {formatDate} from "../utils";
 import {MonthNames} from "../const";
 
-export const createTripDayItemTemplate = (tripDay) => {
-  const dateValue = formatDate(tripDay.date);
-  const dateTitle = `${MonthNames[tripDay.date.getMonth()].toUpperCase()} ${tripDay.date.getDate()}`;
+export const createTripDayItemTemplate = (dayInMilliseconds, counter) => {
+  const date = new Date(dayInMilliseconds);
+  const dateValue = formatDate(date);
+  const dateTitle = `${MonthNames[date.getMonth()].toUpperCase()} ${date.getDate()}`;
   return (`
       <li class="trip-days__item  day">
           <div class="day__info">
-              <span class="day__counter">${tripDay.counter}</span>
+              <span class="day__counter">${counter}</span>
               <time class="day__date" datetime="${dateValue}">${dateTitle}</time>
           </div>
       </li>
