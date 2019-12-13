749feb428e39361c8191439d490bc42cd1155ad4
Alexandra Samonenko <turborufus@gmail.com>
2019-12-13 05:59:58 +0300

    DayItem -> class

--- src/components/trip-day-item.js
+++ src/components/trip-day-item.js
@@ -1,7 +1,7 @@
-import {formatDate} from "../utils";
-import {MONTH_NAMES} from "../const";
+import {formatDate, createElement} from "../utils.js";
+import {MONTH_NAMES} from "../const.js";
 
-export const createTripDayItemTemplate = (dayInMilliseconds, counter) => {
+const createTripDayItemTemplate = (dayInMilliseconds, counter) => {
   const date = new Date(dayInMilliseconds);
   const dateValue = formatDate(date);
   const dateTitle = `${MONTH_NAMES[date.getMonth()].toUpperCase()} ${date.getDate()}`;
@@ -14,3 +14,26 @@ export const createTripDayItemTemplate = (dayInMilliseconds, counter) => {
       </li>
   `);
 };
+export default class {
+  constructor(date, counter) {
+    this._date = date;
+    this._counter = counter;
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createTripDayItemTemplate(this._date.getTime(), this._counter);
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate);
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
