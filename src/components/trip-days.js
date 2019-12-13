5bcec8310e43da4c94c21aab717436c2b01d1cfb
Alexandra Samonenko <turborufus@gmail.com>
2019-12-13 06:15:33 +0300

    DayList -> class

--- src/components/trip-days.js
+++ src/components/trip-days.js
@@ -1,5 +1,29 @@
-export const createTripDaysTemplate = () => {
+import {createElement} from "../utils.js";
+
+const createTripDaysTemplate = () => {
   return (`
       <ul class="trip-days"></ul>
   `);
 };
+
+export default class DayList {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createTripDaysTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
