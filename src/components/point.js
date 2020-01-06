30b9258afcc67dece5c3180bc876604a281c18e8
Alexandra Samonenko <turborufus@gmail.com>
2020-01-06 23:36:55 +0300

    Добавлен обработчик onDataChange. Изменена структура класса

--- src/components/point.js
+++ src/components/point.js
@@ -2,39 +2,58 @@ import EventComponent from "./event.js";
 import EventEditComponent from "./event-edit.js";
 import {render, replace, RENDER_POSITION} from "../utils/render.js";
 
+const MODE = {
+  DEFAULT: `default`,
+  EDIT: `edit`
+};
+
 export default class PointController {
-  constructor(container) {
+  constructor(container, onDataChange) {
     this._container = container;
+    this._onDataChange = onDataChange;
+
+    this._eventComponent = null;
+    this._editComponent = null;
+
+    this._mode = MODE.DEFAULT;
+
+    this._onEscKeyDown = this._onEscKeyDown.bind(this);
   }
 
-  render(event) {
-    const onEscKeyDown = (evt) => {
-      const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
+  render(tripEvent) {
+    // const oldEventComponent = this._eventComponent;
+    // const oldEditComponent = this._editComponent;
 
-      if (isEscKey) {
-        replaceEditToEvent();
-        document.removeEventListener(`keydown`, onEscKeyDown);
-      }
-    };
+    this._eventComponent = new EventComponent(tripEvent);
+    this._editComponent = new EventEditComponent(tripEvent);
 
-    const replaceEditToEvent = () => {
-      replace(eventComponent, editComponent);
-    };
-
-    const replaceEventToEdit = () => {
-      replace(editComponent, eventComponent);
-    };
-
-    const eventComponent = new EventComponent(event);
-    eventComponent.setRollupButtonHandler(() => {
-      replaceEventToEdit();
-      document.addEventListener(`keydown`, onEscKeyDown);
+    this._eventComponent.setRollupButtonHandler(() => {
+      this._replaceEventToEdit();
+      document.addEventListener(`keydown`, this._onEscKeyDown);
     });
 
-    const editComponent = new EventEditComponent(event);
-    editComponent.setSaveButtonHandler(replaceEditToEvent);
-    editComponent.setResetButtonHandler(replaceEditToEvent);
+    this._editComponent.setSaveButtonHandler(this._replaceEditToEvent);
+    this._editComponent.setResetButtonHandler(this._replaceEditToEvent);
 
-    render(this._container, eventComponent.getElement(), RENDER_POSITION.BEFOREEND);
+    render(this._container, this._eventComponent.getElement(), RENDER_POSITION.BEFOREEND);
+  }
+
+  _replaceEditToEvent() {
+    replace(this._eventComponent, this._editComponent);
+    this._mode = MODE.DEFAULT;
+  }
+
+  _replaceEventToEdit() {
+    replace(this._editComponent, this._eventComponent);
+    this._mode = MODE.EDIT;
+  }
+
+  _onEscKeyDown(evt) {
+    const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
+
+    if (isEscKey) {
+      this._replaceEditToEvent();
+      document.removeEventListener(`keydown`, this._onEscKeyDown);
+    }
   }
 }
