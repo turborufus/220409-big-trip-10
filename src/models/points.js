98fd26181fb1d7f50e060d9eb0a17321d27dd8af
Alexandra Samonenko <turborufus@gmail.com>
2020-01-12 13:42:18 +0300

    Создан класс для работы с точками маршрута

---
+++ src/models/points.js
@@ -0,0 +1,13 @@
+export default class Points {
+  constructor() {
+    this._points = [];
+  }
+
+  getPoints() {
+    return this._points;
+  }
+
+  setPoints(points) {
+    this._points = points;
+  }
+}
