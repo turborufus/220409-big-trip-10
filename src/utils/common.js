88afcaffd7cbd4010c4b2166ecd65f547f048d52
Alexandra Samonenko <turborufus@gmail.com>
2020-01-26 21:44:05 +0300

    Функция генерации ID

--- src/utils/common.js
+++ src/utils/common.js
@@ -6,3 +6,8 @@ export const getRandomArrayItem = (array) => {
 export const getRandomIntegerNumber = (min, max) => {
   return Math.floor(min + Math.random() * (max + 1 - min));
 };
+
+export const generateID = () => {
+  const id = `f${(+new Date()).toString(16)}`;
+  return id;
+};
